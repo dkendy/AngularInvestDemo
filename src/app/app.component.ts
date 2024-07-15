af64fb1f8b86ecd0085a39aa4ef8a00e5956cda7
Davi Yorozuya <davi.kendy@hotmail.com>
2024-07-15 17:13:38 -0300

    New Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormComponent } from './form/form.component';
 import { HeaderComponent } from './header/header.component';
 import { TableresultComponent } from './tableresult/tableresult.component';
-
+import { InvestimentParameters } from './Services/model'
 
 @Component({
     selector: 'app-root',
@@ -10,4 +10,7 @@ import { TableresultComponent } from './tableresult/tableresult.component';
     templateUrl: './app.component.html',
     imports: [TableresultComponent, FormComponent, HeaderComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+    
+
+}
